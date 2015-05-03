87b80f9631e9f2af61ea01423df74dc830f06d0b
lucageatti <lucageatti@users.noreply.github.com>
2015-05-03 20:47:07 +0200

    Full exchange of a packet

--- src/big/net/Test.java
+++ src/big/net/Test.java
@@ -21,7 +21,7 @@ public class Test {
         it.add(ctrl_host);
         Control ctrl_stackNode = new Control("stackNode", true, 2);
         it.add(ctrl_stackNode);
-        Control ctrl_domain = new Control("domain", true, 2);
+        Control ctrl_domain = new Control("domain", true, 1);
         it.add(ctrl_domain);
         Control ctrl_firewall = new Control("firewall", true, 1);
         it.add(ctrl_firewall);
@@ -29,88 +29,159 @@ public class Test {
         it.add(ctrl_packet);
         Control ctrl_payload = new Control("payload", true, 0);
         it.add(ctrl_payload);
+        Control ctrl_router = new Control("router", true, 0);
+        it.add(ctrl_router);
         signature = new Signature(it);
-
-        //Http packet exchange:
+        
+        
+        /*
+         * -----------------------------------------------------
+         * Http packet exchange
+         * -----------------------------------------------------
+         */
         BigraphBuilder builder = new BigraphBuilder(signature);
         Root r = builder.addRoot();
-        Node domain = builder.addNode("domain", r);
-        OuterName http_id_server = builder.addOuterName("http_id_server");
-        OuterName tcp_id_server = builder.addOuterName("tcp_id_server");
-        OuterName ipv4_id_server = builder.addOuterName("ipv4_id_server");
-        OuterName eth_id_server = builder.addOuterName("eth_id_server");
-        OuterName http_id_client = builder.addOuterName("http_id_client");
-        OuterName tcp_id_client = builder.addOuterName("tcp_id_client");
-        OuterName ipv4_id_client = builder.addOuterName("ipv4_id_client");
-        OuterName eth_id_client = builder.addOuterName("eth_id_client");
+        OuterName localLinkC = builder.addOuterName("localLinkC");
+        OuterName localLinkS = builder.addOuterName("localLinkS");
+        Node bigDomain = builder.addNode("domain",r);
+        bigDomain.attachProperty(new SharedProperty<String>
+        						( new SimpleProperty<String>("DomainName","BigDomain") ));
+        Node myDomain = builder.addNode("domain", bigDomain);
+        myDomain.attachProperty(new SharedProperty<String>
+								( new SimpleProperty<String>("DomainName","myDomain") ));
+        Node serverDomain = builder.addNode("domain", bigDomain);
+        serverDomain.attachProperty(new SharedProperty<String>
+								( new SimpleProperty<String>("DomainName","googleDomain") ));
+        //Client's outernames
+        OuterName http_ic = builder.addOuterName("http_id_client");
+        OuterName tcp_ic = builder.addOuterName("tcp_id_client");
+        OuterName ipv4_ic = builder.addOuterName("ipv4_id_client");
+        OuterName eth_ic = builder.addOuterName("eth_id_client");
+        OuterName eth_dc = builder.addOuterName("eth_down_client");
+        //RouterClient's outername
+        OuterName ipv4_irci = builder.addOuterName("ipv4_router_client_in");
+        OuterName eth_irci = builder.addOuterName("eth_id_router_client_in");
+        OuterName ipv4_irco = builder.addOuterName("ipv4_router_client_out");
+        OuterName eth_irco = builder.addOuterName("eth_id_router_client_out");
         
-        //Server
-        Node server = builder.addNode("host", domain);
-        server.attachProperty(new SharedProperty<String>(
-				new SimpleProperty<String>("HostName","server_google")));
-        Node http_server = builder.addNode("stackNode", server, http_id_server, tcp_id_server);
-        http_server.attachProperty(new SharedProperty<String>(
-				new SimpleProperty<String>("ProtocolName","http_server_google")));
-        Node tcp_server = builder.addNode("stackNode", server, tcp_id_server, ipv4_id_server);
-        tcp_server.attachProperty(new SharedProperty<String>(
-				new SimpleProperty<String>("ProtocolName","tcp_server_google")));
-        Node ipv4_server = builder.addNode("stackNode", server, ipv4_id_server, eth_id_server);
-        ipv4_server.attachProperty(new SharedProperty<String>(
-				new SimpleProperty<String>("ProtocolName","ipv4_server_google")));
-        Node eth_server = builder.addNode("stackNode", server, eth_id_server, eth_id_client);
-        eth_server.attachProperty(new SharedProperty<String>(
-				new SimpleProperty<String>("ProtocolName","eth_server_google")));
+        //Server's outernames
+        OuterName http_is = builder.addOuterName("http_id_server");
+        OuterName tcp_is = builder.addOuterName("tcp_id_server");
+        OuterName ipv4_is = builder.addOuterName("ipv4_id_server");
+        OuterName eth_is = builder.addOuterName("eth_id_server");
+        OuterName eth_ds = builder.addOuterName("eth_down_server");
+        //RouterServer's outername
+        OuterName ipv4_irsi = builder.addOuterName("ipv4_id_router_google_in");
+        OuterName eth_irsi = builder.addOuterName("eth_id_router_google_in");
+        OuterName ipv4_irso = builder.addOuterName("ipv4_id_router_google_out");
+        OuterName eth_irso = builder.addOuterName("eth_id_router_google_out");
+        
+        //LINK ROUTER
+        OuterName ipv4_irc = builder.addOuterName("ipv4_irc");
+        OuterName ipv4_irs = builder.addOuterName("ipv4_irs");
+        OuterName linkIPRouters = builder.addOuterName("LinkIPRouters");
         
         //Client
-        Node client = builder.addNode("host", domain);
+        Node client = builder.addNode("host", myDomain);
         client.attachProperty(new SharedProperty<String>(
 				new SimpleProperty<String>("HostName","myClient")));
-        Node http_client = builder.addNode("stackNode", client, http_id_client, tcp_id_client);
+        Node http_client = builder.addNode("stackNode", client, http_ic, tcp_ic);
         http_client.attachProperty(new SharedProperty<String>(
 				new SimpleProperty<String>("ProtocolName","http_my_client")));
-        Node tcp_client = builder.addNode("stackNode", client, tcp_id_client, ipv4_id_client);
+        Node tcp_client = builder.addNode("stackNode", client, tcp_ic, ipv4_ic);
         tcp_client.attachProperty(new SharedProperty<String>(
 				new SimpleProperty<String>("ProtocolName","tcp_my_client")));
-        Node ipv4_client = builder.addNode("stackNode", client, ipv4_id_client, eth_id_client);
+        Node ipv4_client = builder.addNode("stackNode", client, ipv4_ic, localLinkC);
         ipv4_client.attachProperty(new SharedProperty<String>(
 				new SimpleProperty<String>("ProtocolName","ipv4_my_client")));
-        Node eth_client = builder.addNode("stackNode", client, eth_id_client, eth_id_server);
-        eth_client.attachProperty(new SharedProperty<String>(
-				new SimpleProperty<String>("ProtocolName","eth_my_client")));
+        //Node eth_client = builder.addNode("stackNode", client, eth_ic, link1);
+        //eth_client.attachProperty(new SharedProperty<String>(
+				//new SimpleProperty<String>("ProtocolName","eth_my_client")));
+        
+        //Client's router
+        Node ipv4_router_client = builder.addNode("stackNode", myDomain, linkIPRouters, localLinkC);
+        ipv4_router_client.attachProperty(new SharedProperty<String>(
+        		new SimpleProperty<String>("Router","ipv4_router_client")));
+        
+        
+        
+        //Server
+        Node server = builder.addNode("host", serverDomain);
+        server.attachProperty(new SharedProperty<String>(
+				new SimpleProperty<String>("HostName","server_google")));
+        Node http_server = builder.addNode("stackNode", server, http_is, tcp_is);
+        http_server.attachProperty(new SharedProperty<String>(
+				new SimpleProperty<String>("ProtocolName","http_server_google")));
+        Node tcp_server = builder.addNode("stackNode", server, tcp_is, ipv4_is);
+        tcp_server.attachProperty(new SharedProperty<String>(
+				new SimpleProperty<String>("ProtocolName","tcp_server_google")));
+        Node ipv4_server = builder.addNode("stackNode", server, ipv4_is, localLinkS);
+        ipv4_server.attachProperty(new SharedProperty<String>(
+				new SimpleProperty<String>("ProtocolName","ipv4_server_google")));
+        //Node eth_server = builder.addNode("stackNode", server, eth_is, link1);
+        //eth_server.attachProperty(new SharedProperty<String>(
+				//new SimpleProperty<String>("ProtocolName","eth_server_google")));
+        
+        //Server's Router
+        Node ipv4_router_server = builder.addNode("stackNode", serverDomain, linkIPRouters, localLinkS);
+        ipv4_router_server.attachProperty(new SharedProperty<String>(
+        		new SimpleProperty<String>("Router","ipv4_router_server")));
+       
         
         //Packet
-        Node http_packet = builder.addNode("packet", client, http_id_client, http_id_server);
+        Node http_packet = builder.addNode("packet", client, http_ic, http_is);
         http_packet.attachProperty(new SharedProperty<String>(
 				new SimpleProperty<String>("PacketName","http_packet")));
         Node http_payload = builder.addNode("payload", http_packet);
         http_payload.attachProperty(new SharedProperty<String>(
 				new SimpleProperty<String>("PayloadName","http_payload")));
         
+        
         Bigraph bigraph = builder.makeBigraph();
         BigPPrinterVeryPretty pp = new BigPPrinterVeryPretty();
         System.out.println(pp.prettyPrint(bigraph, "Bigrafo iniziale"));
         
         
+        
+        /*
+         * ---------------------------------------------------------------
+         * Start of the reactions.
+         * ---------------------------------------------------------------
+         */
+        
         Matcher matcher = new Matcher();
         EncapRule encap = new EncapRule(EncapRule.getRedex(signature),
                 						EncapRule.getReactum(signature), 
                 						new InstantiationMap(2, 0, 1));
-        while (matcher.match(bigraph, EncapRule.getRedex(signature)).iterator().hasNext()
-        		&& !matcher.match(bigraph, ForwardRule.getRedex(signature)).iterator().hasNext()) {
+        while (matcher.match(bigraph, EncapRule.getRedex(signature)).iterator().hasNext() ) {
+        	if( matcher.match(bigraph, DFRule.getRedex(signature)).iterator().hasNext() ){
+        		break;
+        	}
             Iterator<Bigraph> iterator = encap.apply(bigraph).iterator();
             bigraph = iterator.next();
             EncapRule.clearAuxProperties(bigraph);
             System.out.println(pp.prettyPrint(bigraph, "Encap"));
         }
-        ForwardRule forward = new ForwardRule(ForwardRule.getRedex(signature),
-        									ForwardRule.getReactum(signature), 
-        									new InstantiationMap(1, 0));
-        while (matcher.match(bigraph, ForwardRule.getRedex(signature)).iterator().hasNext()) {
-            Iterator<Bigraph> iterator = forward.apply(bigraph).iterator();
+        DFRule ip_forward = new DFRule(DFRule.getRedex(signature),
+        							DFRule.getReactum(signature), 
+        							new InstantiationMap(5, 0, 1, 2, 3, 4));
+        if (matcher.match(bigraph, DFRule.getRedex(signature)).iterator().hasNext()) {
+            Iterator<Bigraph> iterator = ip_forward.apply(bigraph).iterator();
             bigraph = iterator.next();
-            ForwardRule.clearAuxProperties(bigraph);
-            System.out.println(pp.prettyPrint(bigraph, "Forward"));
+            DFRule.clearAuxProperties(bigraph);
+            System.out.println(pp.prettyPrint(bigraph, "Direct Forward"));
         }
+        
+        Domain2HostRule d2h = new Domain2HostRule( Domain2HostRule.getRedex(signature),
+        						Domain2HostRule.getReactum(signature),
+        						new InstantiationMap(3, 0, 1, 2) );
+        if( matcher.match(bigraph, Domain2HostRule.getRedex(signature)).iterator().hasNext() ){
+        	Iterator<Bigraph> iterator = d2h.apply(bigraph).iterator();
+        	bigraph = iterator.next();
+        	Domain2HostRule.clearAuxProperties(bigraph);
+        	System.out.println(pp.prettyPrint(bigraph, "Domain2Host Rule"));
+        }
+        
         DecapRule decap = new DecapRule(DecapRule.getRedex(signature),
         								DecapRule.getReactum(signature),
         								new InstantiationMap(1,0));
@@ -120,7 +191,7 @@ public class Test {
             DecapRule.clearAuxProperties(bigraph);
             System.out.println(pp.prettyPrint(bigraph, "Decap"));
         }
-
+		
         for (Node n : bigraph.getNodes()) {
             StringBuilder sb = new StringBuilder();
             sb.append(n.toString()).append(" ");
@@ -131,69 +202,11 @@ public class Test {
             }
             System.out.println(sb.toString());
         }
-
+        
     }
-
-    protected static void runTest2() {
-        LinkedList<Control> it = new LinkedList<>();
-        Control ctrl_host = new Control("host", true, 0);
-        it.add(ctrl_host);
-        Control ctrl_stackNode = new Control("stackNode", true, 2);
-        it.add(ctrl_stackNode);
-        Control ctrl_domain = new Control("domain", true, 0);
-        it.add(ctrl_domain);
-        Control ctrl_firewall = new Control("firewall", true, 1);
-        it.add(ctrl_firewall);
-        Control ctrl_packet = new Control("packet", true, 2);
-        it.add(ctrl_packet);
-        Control ctrl_payload = new Control("payload", true, 0);
-        it.add(ctrl_payload);
-        signature = new Signature(it);
-
-        //Http packet exchange:
-        BigraphBuilder builder = new BigraphBuilder(signature);
-        Root r = builder.addRoot();
-        Node domain = builder.addNode("domain", r);
-        //Server
-        Node server = builder.addNode("host", domain);
-        OuterName http_id_server = builder.addOuterName("http_id_server");
-        OuterName tcp_id_server = builder.addOuterName("tcp_id_server");
-        OuterName tcp_down_server = builder.addOuterName("tcp_down_server");
-        Node http_server = builder.addNode("stackNode", server, http_id_server, tcp_id_server);
-        Node tcp_server = builder.addNode("stackNode", server, tcp_id_server, tcp_down_server);
-
-        server.attachProperty(new SharedProperty(new SimpleProperty<>("Name", "Server Google")));
-        http_server.attachProperty(new SharedProperty(new SimpleProperty<>("Name", "Strato HTTP del Server")));
-        http_server.attachProperty(new SharedProperty(new SimpleProperty<>("Port0Name", "ID")));
-        http_server.attachProperty(new SharedProperty(new SimpleProperty<>("Port1Name", "DOWN")));
-        tcp_server.attachProperty(new SharedProperty(new SimpleProperty<>("Name", "Strato TCP del Server")));
-
-        //Client
-        Node client = builder.addNode("host", domain);
-        OuterName http_id_client = builder.addOuterName("http_id_client");
-        OuterName tcp_id_client = builder.addOuterName("tcp_id_client");
-        OuterName tcp_down_client = builder.addOuterName("tcp_down_client");
-        Node http_client = builder.addNode("stackNode", client, http_id_client, tcp_id_client);
-        Node tcp_client = builder.addNode("stackNode", client, tcp_id_client, tcp_down_client);
-        //Packet
-        Node http_packet = builder.addNode("packet", client, http_id_client, http_id_server);
-        Node http_payload = builder.addNode("payload", http_packet);
-        //TCP link
-        builder.relink(tcp_server.getPort(0), tcp_client.getPort(1));
-        builder.relink(tcp_server.getPort(1), tcp_client.getPort(0));
-
-        client.attachProperty(new SharedProperty(new SimpleProperty<>("Name", "Client")));
-        http_client.attachProperty(new SharedProperty(new SimpleProperty<>("Name", "Strato HTTP del Client")));
-        tcp_client.attachProperty(new SharedProperty(new SimpleProperty<>("Name", "Strato TCP del Client")));
-        http_payload.attachProperty(new SharedProperty(new SimpleProperty<>("Description", "Payload di un pacchetto HTTP GET")));
-
-        Bigraph bigraph = builder.makeBigraph();
-
-        BigPPrinterVeryPretty pp = new BigPPrinterVeryPretty();
-        //System.out.println(prettyPrint(bigraph));
-        System.out.println(pp.prettyPrint(bigraph, "Test bigraph"));
-
-    }
+    
+    
+    
     
     
     

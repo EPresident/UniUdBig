b6939d15ea2fce92168f9528fee32faac3770281
EPresident <prez_enquiry@hotmail.com>
2015-08-28 16:25:44 +0200

    Removed TODO from MultiMatcher, added license.

--- src/big/match/MultiMatcher.java
+++ src/big/match/MultiMatcher.java
@@ -1,8 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 Elia Calligaris <calligaris.elia@spes.uniud.it> 
+ * and Luca Geatti <geatti.luca@spes.uniud.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package big.match;
 
 import it.uniud.mads.jlibbig.core.std.Bigraph;
@@ -10,8 +23,6 @@ import it.uniud.mads.jlibbig.core.std.Match;
 
 /**
  * Interface that allows to match a host with multiple guests.
- *
- * TODO modificare lista di liste in lista di match con redex
  * 
  * @author EPresident <prez_enquiry@hotmail.com>
  */
